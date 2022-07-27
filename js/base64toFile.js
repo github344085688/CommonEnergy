84eccfeab568b41a62e76ec03116a23acf37f753
qingzhong <qingzhong.feng@unisco.com>
2022-07-27 14:16:56 +0800

    new

--- js/base64toFile.js
+++ js/base64toFile.js
@@ -1,13 +1,28 @@
-base64toFile (dataurl, filename = '') {
-  var arr = dataurl.split(',')
-  var mime = arr[0].match(/:(.*?);/)[1]
-  var suffix = mime.split('/')[1]
-  var bstr = atob(arr[1])
-  var n = bstr.length
-  var u8arr = new Uint8Array(n)
+base64ToFile(base, filename = ''){
+  let arr = base.split(',');
+  let mime = arr[0].match(/:(.*?);/)[1];
+  let suffix = mime.split('/')[1];
+  let bstr = atob(arr[1]);
+  let n = bstr.length;
+  let u8arr = new Uint8Array(n);
   while (n--) {
     u8arr[n] = bstr.charCodeAt(n)
   }
   return new File([u8arr], `${filename}.${suffix}`, {
     type: mime
   });
+}
+
+fileTobase64(file){
+  return new Promise(function (resolve, reject) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (reader) {
+      resolve(reader.currentTarget.result);
+    };
+    reader.onerror = function (error) {
+      reject(error);
+    };
+  });
+}
+
